bb0c40a9c3e46e2138bb0dea2e68231274b57f6c
Fabrice <Fabrice>
2021-12-09 14:41:07 +0100

    doing js the first task

---
+++ js_basics/sript.js
@@ -0,0 +1,46 @@
+console.log ("Hallo Jungs");
+
+let a= 7;
+let b= 9;
+let c= a + b;
+let d= a + b + c;
+
+console.log ("Der Wert a ist " +a)
+console.log ("Der Wert b ist " +b)
+
+console.log ("a + b " +c)
+console.log ("a+b+c " +d)
+
+let f= (a+b)/c;
+
+console.log ("a + b /c ist " +f)
+
+let j = (c-a) +b;
+
+console.log ("c-a +b ist " +j)
+
+let result = a+b+c*3;
+
+console.log ("a+b+c*3 ist " +result)
+
+let g = a+b+c /4
+
+console.log ("Da Ergebnis ist " +g)
+
+a = 3;
+
+b = a + 3;
+
+console.log ("das Ergebnis ist " +b)
+
+ c = c-3;
+ 
+ console.log ("The Result is " +c)
+
+ 
+
+
+
+
+
+
