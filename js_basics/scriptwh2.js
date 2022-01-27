519450f2ee66d1a9bca1bde1ee57eb31d2262965
Fabrice <Fabrice>
2022-01-27 14:30:13 +0100

    test

---
+++ js_basics/scriptwh2.js
@@ -0,0 +1,14 @@
+let a = 17.4;
+let b = 3.9;
+let c = a / b;
+
+
+console.log ("The result of" + a + "divided" / b +  "is" +c );
+
+-
+
+let hello;= " Fabrice ";
+let lastname = " Trinkl ";
+let full = hello + lastname;
+
+console.log ("Ich heiße " +full );
