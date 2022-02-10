1fe7115a063586f943c9fb1cfbd432df1338c085
Fabrice <Fabrice>
2022-02-10 13:55:58 +0100

    script5 finished

--- script5.js
+++ script5.js
@@ -1,18 +1,15 @@
-let number = 19    
+let number = 80
 
 
-if (number<20) {
-    console.console.log("mini");
-}
-
-else if( number>20 <50) {
-    console.log("Medium " +number)
-}
-
-else (number>50) {
-    console.log("large" +number )
+if (number < 20) {
+    console.log("mini");
+} else if (number > 20 && number < 50) {
+    console.log("Medium " + number)
+} else  {
+    console.log("large" + number)
 
 }
-   
+
+
 
 
