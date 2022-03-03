3a8dd050219956ffa0f279d4f20265bd09b0fb04
Fabrice <Fabrice>
2022-03-03 14:46:20 +0100

    excercise 9

---
+++ script9.js
@@ -0,0 +1,33 @@
+let output = "";
+let isX = true
+
+for (let index = 0; index < 20; index++) {
+    if (index % 4 == 0) {
+        console.log(output);
+        output = ""
+
+        if (isX == true) {
+            isX = false
+
+        }
+        else {
+            isX = true
+        }
+
+    }
+
+}
+if (isX == true) {
+
+    output = output + "X";
+}
+else {
+    output = output + "O";
+}
+
+
+
+
+
+
+
