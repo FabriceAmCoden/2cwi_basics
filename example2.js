95379cf62be2f7429fa11814525221e8b8a7ad49
Fabrice <Fabrice>
2022-06-09 14:37:47 +0200

    examples new

--- example2.js
+++ example2.js
@@ -1,13 +1,7 @@
-for (let count = 0; Count>4; count++ ) {
-    console.log(count)
-   }
-    
-   let random = Math.random() * 100;
-
-let randomAsInteger = Math.round(random);
-
-console.log(randomAsInteger);
+let random = Math.random() * 100;
+    let randomAsInteger = Math.round(random);
+    console.log(randomAsInteger);
 
 
-
-   
+  for (let count =randomAsInteger; count >= 0; count--) 
+  console.log(random)
--- example3.js
+++ example3.js
@@ -1,14 +1,10 @@
-for (let count = 10; count >0; count--) {
+let output = 0;
+for (let count = 10; count > 0; count--) {
     let random = Math.random() * 100;
     let randomAsInteger = Math.round(random);
-
     console.log(randomAsInteger);
     output = output + randomAsInteger
 
 }
 
-console.log(output)
-
-if {
-    
-}
+console.log(" Die Summe der 10 Zufallszahlen ist " + output)
