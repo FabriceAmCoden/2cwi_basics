ab253f9ae948a1b52f817468096d1550babf84fb
Fabrice <Fabrice>
2022-06-09 14:20:45 +0200

    examples

---
+++ example2.js
@@ -0,0 +1,13 @@
+for (let count = 0; Count>4; count++ ) {
+    console.log(count)
+   }
+    
+   let random = Math.random() * 100;
+
+let randomAsInteger = Math.round(random);
+
+console.log(randomAsInteger);
+
+
+
+   
---
+++ example3.js
@@ -0,0 +1,10 @@
+for (let count = 10; count >0; count--) {
+    let random = Math.random() * 100;
+    let randomAsInteger = Math.round(random);
+
+    console.log(randomAsInteger);
+    output = output + randomAsInteger
+
+}
+
+console.log(output)
